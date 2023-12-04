f80ecdb47e28b77a3299bd6a626f2068be3284bf
Fabiano Santos <fabianotmp@gmail.com>
2023-12-04 18:35:55 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,14 +7,19 @@ hamburguer.addEventListener("click", () => {
 })
 
 function toggleAside() {
+    var section = document.querySelector('section')
     var aside = document.getElementById('aside');
 
     if (aside.classList.contains('mostrar')) {
         aside.classList.remove('mostrar');
         aside.classList.add('esconder');
+        section.style.marginLeft = '5px'
+
     } else {
         aside.classList.remove('esconder');
         aside.classList.add('mostrar');
+        section.style.marginLeft = '300px'
+
     }
 }
 
